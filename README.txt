53b5d86950194206d14426a4eeafd8f3be029bdb
Piotr Skamruk <piotr.skamruk@gmail.com>
2013-07-11 12:42:36 +0200

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,3 @@
+Routerboard API for accessing mikrotik routers.
+
+Base of this code is http://wiki.mikrotik.com/index.php?title=Manual:API
---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as file:
+    README = file.read()
+with open(os.path.join(here, 'VERSION.txt')) as file:
+    VERSION = file.readline().strip()
+
+setup(
+    name='rosapi',
+    version=VERSION,
+    description='Routerboard API',
+    long_description=README,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Systems Administration',
+    ],
+    author='Piotr Skamruk',
+    author_email='piotr.skamruk@gmail.com',
+    keywords='mikrotik routerboard ros api',
+    packages=find_packages(),
+    zip_safe=True,
+)
